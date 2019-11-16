dbd855bbcf2c9587c2f6f1b71253189bfb01f077
Remigijus <achmudass@gmail.com>
2019-11-16 18:57:43 +0200

    Using Eureka for currency exchange service

---
+++ src/test/java/co/kurapka/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,14 @@
+package co.kurapka.microservices.netflixeurekanamingserver;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
